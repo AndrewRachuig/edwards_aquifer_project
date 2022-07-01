116746bf8f94a46dfa3d1a78ec0bcc05d20975c3
Andrew Rachuig <andrew.rachuig@gmail.com>
2022-06-30 22:20:39 -0500

    explore.py created

---
+++ explore.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import scipy.stats as stats
+import pylab
+import statsmodels.api as sm
+
+plt.rcParams["figure.figsize"] = (14, 8)
+
+import wrangle
+
